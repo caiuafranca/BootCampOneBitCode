c52f091f66b53fda7324d7c7b5d9ded77a70049a
Caiua França <caiuagomes@gmail.com>
2018-05-09 15:29:54 -0300

    Ajustes no exercicio

--- cursoRuby/lista_atividades_1.rb
+++ cursoRuby/lista_atividades_1.rb
@@ -1,4 +1,4 @@
-arr = ["Caiua", "Nadja", "Hiram", "Sol"]
+arr = [{nome: "Caiua"},{nome: "Nadja"},{nome: "Hiram"},{nome: "Sol"}]
 
 puts arr
 
@@ -6,13 +6,18 @@ puts "você deseja adicionar alguem na lista? s/n"
 resposta = gets.chomp
 
 while resposta == 's' do
-    puts "Digite o nome do integrante:"    
-    arr.push(gets.chomp)
+    puts "Digite o nome do integrante:"
+    nom = gets.chomp
+    puts "Digite o idade do integrante:"
+    idade = gets.chomp
+    puts "Digite o sexo do integrante:"
+    sexo = gets.chomp    
+    arr.push({nome: nom, idade: idade, sexo: sexo})
 
     puts "você deseja adicionar alguem na lista? s/n"
     resposta = gets.chomp
 end
 
 arr.each do |nome|
-    puts nome
+    puts nome[:nome]
 end
